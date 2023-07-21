bec884c0875e595f23e68673a8dbe3f190867525
jssimas <65747927+jssimas@users.noreply.github.com>
2023-07-21 20:03:35 -0300

    Iniciando exercícios da semana 3

---
+++ Exercícios Semana 02/Projeto_Semana3/index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const fs = require("fs");
+
+const servidor = http.createServer((request, response) => {
+   
+  });
+  
+  servidor.listen(3000);
+  
+  console.log("Servidor rodando na porta http://localhost:3000");
